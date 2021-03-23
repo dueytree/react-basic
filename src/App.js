d16642814dd901005c1a7a543fb61cea0e0aab6f
dueytree <dueytree.00@gmail.com>
2021-03-23 17:44:54 +0900

    create react-app and setting

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Button } from "antd";
+import "App.css";
+
+class Counter1 extends React.Component {
+  state = {
+    value: this.props.initialValue,
+  };
+
+  onClick = () => {
+    const { value } = this.state;
+    this.setState({ value: value + 1 });
+  };
+
+  render() {
+    const { value } = this.state;
+    return (
+      <div>
+        Counter1: {value}
+        <Button onClick={this.onClick}>+1</Button>
+      </div>
+    ); // jsx 문법
+  }
+}
+
+class FruitComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>좋아하는 과일</h1>
+        <ul>
+          {this.props.fruits.map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+function App() {
+  const fruits = ["바나나", "사과", "딸기"];
+  return (
+    <div>
+      <Counter1 initialValue={10} />
+      <FruitComponent fruits={fruits} />
+    </div>
+  );
+}
+
+export default App;
